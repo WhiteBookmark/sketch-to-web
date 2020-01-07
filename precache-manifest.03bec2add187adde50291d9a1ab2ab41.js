072708cfa16cc0c3a90569e9258ae979d048101c
Suleman Khan <suleman.mehmood@yahoo.com>
2020-01-07 21:48:41 +0500

    deploy

---
+++ precache-manifest.03bec2add187adde50291d9a1ab2ab41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a0fd81e1e6d80a662c",
+    "url": "/sketch-to-web/css/chunk-vendors.4ba75e08.css"
+  },
+  {
+    "revision": "40c10f738c4f154c4aabe405916479fc",
+    "url": "/sketch-to-web/index.html"
+  },
+  {
+    "revision": "9b45ddef4a500c772fcc",
+    "url": "/sketch-to-web/js/app.d1bf7569.js"
+  },
+  {
+    "revision": "95a0fd81e1e6d80a662c",
+    "url": "/sketch-to-web/js/chunk-vendors.98bd0d71.js"
+  },
+  {
+    "revision": "958f8f8bb7ec0b67acc5c9ac0131db2c",
+    "url": "/sketch-to-web/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sketch-to-web/robots.txt"
+  }
+]);
